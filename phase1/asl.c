b6fe98e3cc27ae316e8a91c101a60edaa7e0770b
Landon Clark <landonclark97@gmail.com>
2019-09-03 14:36:23 -0400

    Edited types.h to include pcb_PTR, added outline to asl.c

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,3 +1,30 @@
 /* currently a dummy file, planned to contain the methods for the Active
 Semephore List. The struct for this is a LinkedList of integers each
 associated with an address in memory and a process queue.*/
+
+#include "../h/types.h"
+#include "../h/const.h"
+
+int insertBlocked(int *semAdd, pcb_t *p){
+
+}
+
+
+pcb_t *removeBlocked(int *semAdd){
+
+}
+
+pcb_t *outBlocked(pcb_t *p){
+
+}
+
+pcb_t *headBlocked(int *semAdd){
+
+}
+
+/* Initialize the semdFree list to contain all the elements of the array
+static semd_t semdTable[MAXPROC]. This method will be only called once
+during data structure initialization. */
+initASL(){
+
+}
--- phase1/pcb.c
+++ phase1/pcb.c
@@ -1,19 +1,21 @@
 /* This is currently a dummy file for methods relating to the manipulation of
 the Process Control Block struct. */
 
-#include "defs.h"
+#include "../h/defs.h"
+#include "../h/const.h"
+static pcb_PTR pcbFree_h;
 
 /******************************ALLOC/DEALLOC PBCS*********************************/
 
 /* Inserts the element pointed to by "p" onto the pcbFree list */
-void freePcb(pcb_t *p){
+void freePcb(pcb_PTR p){
 
 }
 
 /* If pcbFree list is empty, return NULL. Otherwise, remove an element
 from the pcbFree list, initialize values for the pcb's fields (NULL) and
 return a pointer to the removed element. */
-pct_t *allocPcb(){
+pct_PTR allocPcb(){
 
 }
 
@@ -21,26 +23,34 @@ pct_t *allocPcb(){
 x is equal to the MAXPROC constant in const.h. This method should only be called
 once at initialization. */
 initPcbs(){
-
-}
+  static pcb_t pcbArr[MAXPROC];
+  int i = 0;
+  while(i < MAXPROC){
+    freePcb(&(pcbArr[i])); //stores all the new pcbs addresses in the pcbFree_h list.
+    i++;
+  }
 
 /****************************PCB QUEUE MAINTENANCE********************************/
 
 /* Initializes an empty Process Queue by setting a variable to be a tail pointer to a
 new process queue. Returns a tail pointer. */
-pcb_t *mkEmptyProcQ(){
-
+pcb_PTR mkEmptyProcQ(){
+  return(NULL); // returns a place in memory that is a pointer to a empty pcb.
 }
 
 /* A query method that checks whether a given queue is empty. Returns TRUE if the
 pcb_t pointed to by the tail pointer (*tp) is empty. Returns FALSE otherwise. */
-int emptyProcQ(pcb_t *tp){
-
+int emptyProcQ(pcb_PTR tp){
+  if(tp == NULL){ // could also be written (return(tp == NULL)) but this made more
+    return TRUE;  // sense to me.
+  } else {
+    return FALSE;
+  }
 }
 
 /* Inserts the process control block pointed to by "p" into the PCB queue whose tail-
 pointer is pointed to by "tp". */
-insertProcQ(pcb_t **tp, pcb_t *p){
+insertProcQ(pcb_PTR *tp, pcbPTR p){
 
 }
 
@@ -48,20 +58,20 @@ insertProcQ(pcb_t **tp, pcb_t *p){
 Returns NULL if the process queue passed in was already empty, but otherwise returns a
 pointer to the process control block that was removed from the queue. It also updates
 the tail pointer if necessary. */
-pcb_t *removeProcQ(pcb_t **tp){
+pcbPTR removeProcQ(pcb_PTR *tp){
 
 }
 
 /* Removes an element pointed to by "p". This pcb can be located anywhere in the queue.
 Updates the tail pointer of the queue if necessary. Returns NULL if the given address
 cannot be matched in the provided queue, and otherwise returns "p". */
-pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
+pcbPTR outProcQ(pcb_PTR *tp, pcb_PTR p){
 
 }
 
 /* Returns a pointer of the head element of a given pcb queue, but does not remove it
 from the list. */
-pcb_t *headProcQ(pcb_t *tp){
+pcbPTR headProcQ(pcb_PTR tp){
 
 }
 
