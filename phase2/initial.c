0a01f0056cbee4c38e118c17a0b0a51d92d483ef
sellersw <sellerswpatrick@outlook.com>
2019-09-25 12:06:08 -0400

    added structure to initial.c

--- phase2/initial.c
+++ phase2/initial.c
@@ -13,8 +13,33 @@ before passing control to the scheduler.
 
 void main(){
 
-  initPcbs()
-  initASL()
+  /* HERE WE NEED TO POPULATE ROM RESERVED FRAMES. NOT CERTAIN HOW TO ACCOMPLISH THAT YET. */
 
 
+
+
+  /* Initialize the data structures (Process Control Blocks and Active Semephore List) */
+  initPcbs();
+  initASL();
+
+  /* Initialize nucleus-maintained vars */
+  static int procCnt = 0;
+  static int sftBlkCnt = 0;
+  static pcb_PTR readyQue = MkEmptyProcQ;
+  static pcb_PTR currProc = NULL;
+          /* NOTE: THESE MIGHT NEED TO BE INITIALIZED IN THE HEADER TO BE MADE GLOBAL, AND
+          SET IN THE MAIN METHOD. IF NOT, THEY MIGHT FALL OUT OF SCOPE ONCE WE CALL SCHEDULER.
+          CURRENTLY UNCLEAR, SO IM LEAVING IT THIS WAY.
+
+  /* INIT NUCLEUS-MAINTAINED SEMEPHORES */
+  
+
+
+
+  /* INSTANTIATE A PROC AND PLACE IT ON THE READY QUEUE. */
+
+
+
+
+  /* CALL SCHEDULER */
 }
