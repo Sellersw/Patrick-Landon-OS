65750c863ae31ae5fc8ec735e983909cf7426f05
Landon Clark <landonclark97@gmail.com>
2019-10-13 19:20:19 -0400

    Added some things - maybe got control flow of syscallhandler done

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -11,14 +11,25 @@ Module to handle exceptions. More words to follow.
 #include "../e/asl.e"
 
 void sysCallHandler(){
-  unsigned int call, status;
+  unsigned int call, status, mode;
 
   state_t *oldState = (state_t *) SYSCALLOLD;
   status = oldState->s_status;
   call = oldState->s_a0;
 
+  /* Remove any values from status register other than KUc bit
+  mode = status << 30;
+  mode = mode >> 31;
+
+  Not sure if this is necessary or even correct. */
+
   if(call >= 9){
     // Pass up or die
+  }
+
+  /* Not sure if we need to check KUc bit or KUp bit. This is checking KUp */
+  if((status & KERNELOFF) == KERNELOFF){
+    // Handle user mode priveleged instruction
   }
 
 
