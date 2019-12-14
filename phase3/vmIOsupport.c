60e4feeebed16bd80f48d7b9ae6dae4dacfaff06
Landon Clark <landonclark97@gmail.com>
2019-12-14 03:22:58 -0500

    hi

--- phase3/vmIOsupport.c
+++ phase3/vmIOsupport.c
@@ -207,6 +207,7 @@ HIDDEN void writeToTerminal(state_t *state, int asid){
 
     getDeviceReg(TERMINT, asid-1)->t_transm_command = (virtAddr[i] << 8) | TRANSMCHAR;
 
+
     status = SYSCALL(WAITIO, TERMINT, asid-1, 0);
     debugOMICRON(status);
 
