3064c2e6988431afa57be78c629dcf86f324f920
Landon Clark <landonclark97@gmail.com>
2019-12-13 23:24:41 -0500

    idk

--- phase3/vmIOsupport.c
+++ phase3/vmIOsupport.c
@@ -218,10 +218,10 @@ HIDDEN void writeToTerminal(state_t *state, int asid){
   for(i = 0; i < len; i++){
     disableInts(TRUE);
 
+    debugOMICRON(status);
+
     termReg->t_transm_command = (virtAddr[i] << 8) | TRANSMCHAR;
     status = SYSCALL(WAITIO, TERMINT, asid-1, 0);
-
-    debugOMICRON(status);
 
     disableInts(FALSE);
 
