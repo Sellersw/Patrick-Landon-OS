ed7ca01208ee71bbffda0b54b4a0989cff144a3c
Landon Clark <landonclark97@gmail.com>
2019-12-15 13:58:16 -0500

    push

--- phase3/vmIOsupport.c
+++ phase3/vmIOsupport.c
@@ -196,6 +196,8 @@ HIDDEN void writeToTerminal(){
     getDeviceReg(TERMINT, asid-1)->t_transm_command = (virtAddr[i] << 8) | TRANSMCHAR;
     status = SYSCALL(WAITIO, TERMINT, asid-1, 0);
 
+    debugOMICRON(status);
+
     disableInts(FALSE);
 
     if((status & STATUSMASK) != CHARTRANSMD){
